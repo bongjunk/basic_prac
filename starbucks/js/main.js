0a929e7f90d83058a51a3e1a584d8195674ca784
bongjunk <mydlwm_s@naver.com>
2022-01-25 13:03:23 +0900

    0125 수정

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
@@ -45,6 +47,8 @@ fadeEls.forEach(function(fadeEl, index) {
   });
 });
 
+
+// 슬라이드 요소
 // new Swiper(선택자, 옵션)
 new Swiper('.notice-line .swiper-container', {
   direction: 'vertical', // 수직 슬라이드
@@ -53,7 +57,7 @@ new Swiper('.notice-line .swiper-container', {
 });
 
 new Swiper('.promotion .swiper-container', {
-  slidePerView: 3, // 한번에 보여줄 슬라이드 개수
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
   spaceBetween: 10, // 슬라이드 사이 여백
   centerdSlides: true, // 1번 슬라이드가 가운데 보이기
   loop: true,
@@ -70,6 +74,19 @@ new Swiper('.promotion .swiper-container', {
   }
 });
 
+new Swiper('.awards .swiper-container', {
+  // direction: 'horizontal', // 수평 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true, // 반복 재생 여부
+  spaceBetween: 30, // 슬라이드 사이 여백
+  slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
+  // slidesPerGroup: 5, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
+  navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+    prevEl: '.awards .swiper-prev', // 이전 버튼 선택자
+    nextEl: '.awards .swiper-next' // 다음 버튼 선택자
+  }
+})
+
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
@@ -82,4 +99,40 @@ promotionToggleBtn.addEventListener('click', function() {
     // 보임처리
     promotionEl.classList.remove('hide');
   }
+});
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseInt((Math.random() * (max - min) + min).toFixed(2))
+}
+
+function floatingObject(selector, delay, size) {
+  // gsap.to(요소, 시간, 옵션);
+  //        선택자      지속시간
+  gsap.to(selector, random(1.5, 2.5), {
+    // 옵션
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut,
+    delay: random(0, delay)
+  });
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', 5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy')
+spyEls.forEach(function(spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8
+    })
+    //             토글요소, 토글클래스 이름
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
 });
