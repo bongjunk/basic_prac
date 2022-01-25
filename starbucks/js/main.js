f384f587400abd136ca4a6660bc9cd9216b34c07
bongjunk <mydlwm_s@naver.com>
2022-01-25 16:05:39 +0900

    final commit

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -29,14 +29,31 @@ window.addEventListener('scroll', _.throttle(function() {
       opacity: 0,
       display: 'none'
     });
+    // 버튼 보이기
+    gsap.to('#to-top', .2, {
+      x: 0
+    })
+    
   } else {
+    // 배지 보이기
     gsap.to(badgeEl, .6, {
       opacity: 1,
       display: 'block'
     });
+    // 버튼 숨기기
+    gsap.to('#to-top', .2, {
+      x: 100 
+    });
   }
 }, 300));
 // ._throttle(함수, 시간)
+
+const toTopEl = document.querySelector('#to-top');
+toTopEl.addEventListener('click', function() {
+    gsap.to(window, .7, {
+      scrollTo: 0
+    });
+});
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function(fadeEl, index) {
@@ -59,7 +76,7 @@ new Swiper('.notice-line .swiper-container', {
 new Swiper('.promotion .swiper-container', {
   slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
   spaceBetween: 10, // 슬라이드 사이 여백
-  centerdSlides: true, // 1번 슬라이드가 가운데 보이기
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
   loop: true,
   autoplay: {
     delay: 5000
@@ -136,3 +153,6 @@ spyEls.forEach(function(spyEl) {
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear(); // 2022
